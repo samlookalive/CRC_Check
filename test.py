58be06b382ecab46a5f0829984fc44d1ff4b6c2d
samlooklive <lookalive@naver.com>
2023-02-22 10:58:38 +0900

    modify program

--- test.py
+++ test.py
@@ -1,16 +1,24 @@
 import os
 from tkinter import filedialog
 from tkinter import messagebox
-
 import tkinter
 
 polynominal = 0XEDB88320
+tk = tkinter.Tk()
+
+# GUI 설정
+#Set_GUIConfigure(mainWindow)
+tk.iconbitmap("CRC_Check.ico")
+tk.title('CRC32 Calculator')
+tk.geometry('395x150+50+50')
+tk.resizable(False, False)
+tk.option_add('*Font', '맑은고딕 11')
 
 def u32(n):
       return n & 0xFFFFFFFF
 
 def makecrctable():
-      table = list()
+      tb = list()
       for i in range(256) :
             k = i
             for j in range(8) :
@@ -18,40 +26,44 @@ def makecrctable():
                         k = (k >> 1) ^ polynominal
                   else :
                         k >>= 1
-            table.append(k)
-      return table
+            tb.append(k)
+      return tb
 
 def calcCRC(infp):
-      mytable = makecrctable()
+      crctable = makecrctable()
       CRC = u32(~0)
       while (byte := infp.read(1)):
-            CRC = u32(mytable[(CRC ^ int.from_bytes(byte, "big")) & 0XFF ] ^ (CRC >> 8))
+            CRC = u32(crctable[(CRC ^ int.from_bytes(byte, "big")) & 0XFF ] ^ (CRC >> 8))
       return u32(~CRC)
 
-tk = tkinter.Tk()
-tk.title('CRC32 Calculator')
-tk.geometry('300x100+50+50')
-tk.option_add('*Font', '맑은고딕 25')
-files = filedialog.askopenfilename(initialdir="/",\
-                 title = "파일을 선택 해 주세요",\
-                 filetypes = (("all files","*"), ("hex files","*hex"),("binary files","*bin")))
-#files 변수에 선택 파일 경로 넣기
+def OpenFile():
+      try:
+            # Openfile 버튼 입력 시 선택된 파일 정보 return
+            filename = filedialog.askopenfilename(initialdir="/",\
+                  title = "파일을 선택 해 주세요",\
+                  filetypes = (("all files","*"), ("hex files","*hex"),("binary files","*bin")))
+            if filename :
+                  entry_file.delete(0, len(entry_file.get()))
+                  entry_file.insert(0, filename)
+                  entry_crc.delete(0, len(entry_crc.get()))
+                  fp = open(filename, 'rb')
+                  entry_crc.insert(0,"0x"+f'{calcCRC(fp):X}')
+      except:
+            messagebox.showerror("ERROR", "알수 없는 오류 발생.")
 
-if files == '':
-      messagebox.showwarning("경고", "파일을 추가 하세요")    #파일 선택 안했을 때 메세지 출력
-else :
-      label_file = tkinter.Label(tk, text='File Name').grid(row=0, column=0)
-      
-      label_crc = tkinter.Label(tk, text='CRC32 ').grid(row=1, column=0)
+label_crc = tkinter.Label(tk, text='CRC32', width =7, font=("맑은고딕",18))
+label_file = tkinter.Label(tk, text='파일 위치', width=10)
+entry_file = tkinter.Entry(tk, width=35)
+entry_crc = tkinter.Entry(tk, width=11, font=("맑은고딕", 30))
+entry_file.insert(0,"파일을 선택하세요.")
+entry_crc.insert(0," ")
+btn_load = tkinter.Button(tk, text='선택',width=8, command=lambda:OpenFile())
 
-      entry = tkinter.Entry(tk, width=25).grid(row=0, column=1)
-      entry.insert(0,files)
-      #entry.pack(padx=5, pady=5)
-      mytable = range(0,256)
+label_crc.grid(padx=5, pady=20, row = 0, column= 0, sticky='s')
+entry_crc.grid(padx=5, pady=22, row = 0, column= 1, columnspan=3)
 
-      fp = open(files, 'rb')
-      entry_crc = tkinter.Entry(tk, text=hex(calcCRC(fp)),width=25).grid(row=1, column=1)
-      #entry_crc.insert(2, hex(calcCRC(fp)))
-      #entry_crc.pack(padx=5, pady=5)
+label_file.grid(padx=8, pady=1, row = 1, column= 0, sticky='w')
 
-      tk.mainloop()
+entry_file.grid(padx=8, pady=1, row = 2, column= 0, columnspan=3)
+btn_load.grid(padx=1, pady=1, row = 2, column= 3)
+tk.mainloop()
